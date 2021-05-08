754306e56e8a459697fef0eb3ce3fe400f3fe050
Dirga Yasa <cekingx@gmail.com>
2021-05-08 15:16:23 +0800

    auth unit test

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,44 @@
+import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import { UsersService } from '../users/users.service';
 import { AuthService } from './auth.service';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const bcrypt = require('bcrypt');
+jest.mock('bcrypt', () => ({
+  compare: jest.fn(),
+}));
 
 describe('AuthService', () => {
   let service: AuthService;
+  const mockUserService = {
+    id: 1,
+    name: 'name',
+    username: 'username',
+    password: 'password',
+    walletAddress: '0x',
+    privateKey: '0x1',
+    userRole: {
+      role: 'role',
+    },
+    findOne: () => mockUserService,
+  };
+
+  const mockJwtService = {};
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: mockUserService,
+        },
+        {
+          provide: JwtService,
+          useValue: mockJwtService,
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
@@ -14,5 +46,23 @@ describe('AuthService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return valid user', async () => {
+    bcrypt.compare.mockImplementation(() => true);
+    const spy = jest.spyOn(mockUserService, 'findOne');
+    const user = await service.validateUser('username', 'password');
+
+    expect(spy).toHaveBeenCalled();
+    expect(user).toBeTruthy();
+  });
+
+  it("should return null if password doesn't match", async () => {
+    bcrypt.compare.mockImplementation(() => false);
+    const spy = jest.spyOn(mockUserService, 'findOne');
+    const user = await service.validateUser('username', 'password');
+
+    expect(spy).toHaveBeenCalled();
+    expect(user).toBeNull();
   });
 });
--- src/elections/election/election.service.spec.ts
+++ src/elections/election/election.service.spec.ts
@@ -1,12 +1,64 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from '../../users/user.entity';
+import { Connection } from 'typeorm';
+import { Candidate } from '../entity/candidate.entity';
+import { ElectionParticipant } from '../entity/election-participant.entity';
+import { ElectionStatus } from '../entity/election-status.entity';
+import { Election } from '../entity/election.entity';
+import { Misi } from '../entity/misi.entity';
+import { Pengalaman } from '../entity/pengalaman.entity';
 import { ElectionService } from './election.service';
+
+const mockElectionStatusRepository = {};
+const mockElectionRepository = {};
+const mockCandidateRepository = {};
+const mockMisiRepository = {};
+const mockPengalamanRepository = {};
+const mockUserRepository = {};
+const mockElectionParticipantRepository = {};
+const mockConnection = {};
 
 describe('ElectionService', () => {
   let service: ElectionService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ElectionService],
+      providers: [
+        ElectionService,
+        {
+          provide: getRepositoryToken(ElectionStatus),
+          useValue: mockElectionStatusRepository,
+        },
+        {
+          provide: getRepositoryToken(Election),
+          useValue: mockElectionRepository,
+        },
+        {
+          provide: getRepositoryToken(Candidate),
+          useValue: mockCandidateRepository,
+        },
+        {
+          provide: getRepositoryToken(Misi),
+          useValue: mockMisiRepository,
+        },
+        {
+          provide: getRepositoryToken(Pengalaman),
+          useValue: mockPengalamanRepository,
+        },
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockUserRepository,
+        },
+        {
+          provide: getRepositoryToken(ElectionParticipant),
+          useValue: mockElectionParticipantRepository,
+        },
+        {
+          provide: Connection,
+          useValue: mockConnection,
+        },
+      ],
     }).compile();
 
     service = module.get<ElectionService>(ElectionService);
